f7e769dd403e4f935cde67031f599807e3b3c088
Luis Xavier <lpesantesm@gmail.com>
2017-03-13 13:54:22 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,4 +1,4 @@
-?php
+<?php
 //$appName = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 $appName = "psql postgresql-asymmetrical-74036 --app erpproyecto";
 $connStr = "host=ec2-54-243-185-99.compute-1.amazonaws.com port=5432 dbname=dcjbjufidqvphb user=lobxpiyeyxejzq password=cd75b62e495181141f92dece1d64f46ce79ac081479e86ae503060a70024ecb9 options='--application_name=$appName'";
