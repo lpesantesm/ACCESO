a4ad547d7807c71ad9849814d1d4394114d90618
Marcos A. Salinas Sanipatin <>
2017-03-31 06:30:32 -0500

    Bozeto del GRUB de Modulo

---
+++ pages/se/segsemt0101.php
@@ -0,0 +1,75 @@
+<?php
+ $pagsize = 10;
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1> MODULOS DEL SISTEMA <small>En este formulario puede crear, modificar, elimninar o consultar los modulos del sistema.</small> </h1>            
+        </section>
+        <!-- / Content Header (Page header) -->
+        <!-- Main content -->
+        <section class="content">
+            <!-- Horizontal Form -->
+            <div class="box box-info">
+              <div class="box-header with-border">
+                <h3 class="box-title">Horizontal Form</h3>
+              </div>
+              <!-- /.box-header -->
+              <!-- form start -->
+              <form class="form-horizontal">
+                <div class="box-body">
+                  <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+
+                    <div class="col-sm-10">
+                      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+
+                    <div class="col-sm-10">
+                      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"> Remember me
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+                  <button type="submit" class="btn btn-default">Cancel</button>
+                  <button type="submit" class="btn btn-info pull-right">Sign in</button>
+                </div>
+                <!-- /.box-footer -->
+              </form>
+            </div>            
+            
+            
+            
+            
+            <!-- / Horizontal Form -->
+
+            
+        </section>
+        <!-- / Main content -->
+
+            
+        
+        
+        
+        
+    </body>
+</html>
